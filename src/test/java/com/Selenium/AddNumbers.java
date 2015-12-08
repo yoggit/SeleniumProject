33ec12e3832f719589727f94bbd8fe379e0fc90e
yoggit <yogeshthraju@gmail.com>
2015-12-07 23:54:44 -0500

    First Program

---
+++ src/test/java/com/Selenium/AddNumbers.java
@@ -0,0 +1,21 @@
+package com.Selenium;
+
+public class AddNumbers {
+	
+	int Num1 = 0;
+	int Num2 = 0;
+	double Num3 = 0.0;
+	
+	public void setNum1(int Num1){
+		this.Num1 = Num1;
+	}
+	
+	public void setNum2(int Num2){
+		this.Num2 = Num2;
+	} 
+	
+	public double AddNum(){
+		return Num3 = Num1+Num2;
+	}
+}
+
